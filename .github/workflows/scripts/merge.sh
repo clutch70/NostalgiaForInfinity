ab856131aef61ffead8045e73809710979ad8714
clutch70 <67942178+clutch70@users.noreply.github.com>
2022-06-29 17:35:10 -0500

    update merge.sh

--- .github/workflows/scripts/merge.sh
+++ .github/workflows/scripts/merge.sh
@@ -1,7 +1,9 @@
 oldCommit=$(git log --pretty=format:'%h' -n 1)
 echo Collected hash is $oldCommit
-git config --global user.name 'clutch70'
-git config --global user.email 'b.custard@outlook.com'
+echo GHUSER is $GHUSER
+echo GHEMAIL is $GHEMAIL
+git config --global user.name '$GHUSER'
+git config --global user.email '$GHEMAIL'
 git pull --unshallow  # this option is very important, you would get
                       # complains about unrelated histories without it.
                       # (but actions/checkout@v2 can also be instructed
