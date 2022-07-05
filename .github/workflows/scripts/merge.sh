5e38c8340383d9875c14a7a644da20a2fade8b8f
clutch70 <67942178+clutch70@users.noreply.github.com>
2022-07-05 04:06:21 -0500

    Cleanup merge.sh comments and improve logic

--- .github/workflows/scripts/merge.sh
+++ .github/workflows/scripts/merge.sh
@@ -1,17 +1,19 @@
+# Get the current commit hash so we can compare later
 oldCommit=$(git log --pretty=format:'%h' -n 1)
-#echo Collected hash is $oldCommit
-#echo GHUSER is $GHUSER
-#echo GHEMAIL is $GHEMAIL
+
+# Set username and email vars
 git config --global user.name $GHUSER
 git config --global user.email $GHEMAIL
 git pull --unshallow  # this option is very important, you would get
                       # complains about unrelated histories without it.
                       # (but actions/checkout@v2 can also be instructed
                       # to fetch all git depth right from the start)
+# Set the upstream repo, fetch, merge, and push
 git remote add upstream https://github.com/iterativv/NostalgiaForInfinity
 git fetch upstream
 git merge upstream/main
 git push origin main
+# Get the new commit hash to compare
 newCommit=$(git log --pretty=format:'%h' -n 1)
 # Delete the next 3 lines if you don't have any other forks.
 # Rename "short" if you do
@@ -19,7 +21,6 @@ git checkout short
 git merge upstream/main
 git push origin short
 
-#echo Collected hash is $newCommit
 if [[ "$oldCommit" == "$newCommit" ]]
 then
   echo Commit $newCommit is unchanged!!!
@@ -32,5 +33,6 @@ then
   git clone https://github.com/clutch70/pushover
   cd pushover
   echo $POTOKEN > pushover_api
-  python3 pushover.py "NFI Fork Updated" "GH workflows have automatically merged upstream NFI changes to the CN fork." "0"
+  python3 pushover.py "NFI Fork Updated" "GH workflows have automatically merged upstream NFI changes to the CN fork." "-1"
+  exit
 fi
